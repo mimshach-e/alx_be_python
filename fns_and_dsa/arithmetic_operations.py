8868330eec58bc6ed44fe1b79a93c73379e14b11
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-05 12:06:48 +0000

    added new files

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+# Functions, Data Structures and Modules 
+# Task 0 - Arithmetic Operations Function
+
+# Variable Declaration
+num1 = float(0)
+num2 = float(0)
+operation = str("")
+
+# Function Definition
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Division by zero is not allowed!"
+            
+    else:
+        print("Operation not found!")
+
+       
+
+
