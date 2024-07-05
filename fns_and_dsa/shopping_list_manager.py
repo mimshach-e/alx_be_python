674f8cf375f522154fb3e87204d2eacdc6981a1c
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-05 21:51:02 +0000

    made changes to file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -48,7 +48,7 @@ def main():
 
         if choice == '1':
              # Prompt for and add an item
-            item_name = input("Enter the name of the item: ")
+            item_name = input("Enter the item to add: ")
             shopping_list.append(item_name)
             print(f"The item '{item_name}' is added successfully.")  
           
