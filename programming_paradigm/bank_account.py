c4d89f1c31adbf1bfba021e93d26f964dc657d9d
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-13 13:09:07 +0000

    made changes to file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,7 +2,7 @@
 # Task 0 - Create a Simple Bank Account Class
 
 # Class Definition
-class BankAccount():
+class BankAccount:
     def __init__(self, account_balance = 0):
         self.account_balance = account_balance
 
@@ -19,4 +19,4 @@ class BankAccount():
         
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
-            
+
