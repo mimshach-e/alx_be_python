1ddd7774cbf632dda8fb4682834607adbb958197
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-13 15:56:40 +0000

    made changes to file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,50 +6,54 @@ from simple_calculator import SimpleCalculator
 # Define a Test Class:
 class TestSimpleCalculator(unittest.TestCase):
 
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
 # Write Test Methods:
     def test_add(self):
         # est for edge cases
         try:
           # Use Assertions to Verify Results:
-          self.assertEqual(SimpleCalculator(3, 5), 8)
-          self.assertEqual(SimpleCalculator(-1, 5), 4)
-          self.assertEqual(SimpleCalculator(0, 6), 6)
-          self.assertEqual(SimpleCalculator('a', 4))
-          self.assertEqual(SimpleCalculator(None))
+          self.assertEqual(self.calc.add(3, 5), 8)
+          self.assertEqual(self.calc.add(-1, 5), 4)
+          self.assertEqual(self.calc.add(0, 6), 6)
+          self.assertEqual(self.calc.add("a", 4), 9)
+          self.assertEqual(self.calc.add(None))
         except TypeError as t:
            print(t)
 
     def test_subtract(self):
        try:
          
-         self.assertEqual(SimpleCalculator(4, 1),3)
-         self.assertEqual(SimpleCalculator(-3, -3), 0)
-         self.assertEqual(SimpleCalculator(5, -5), 10)
-         self.assertEqual(SimpleCalculator(10, 0), 10)
-         self.assertEqual(SimpleCalculator(4, 'b'))
-         self.assertEqual(SimpleCalculator(None))
+         self.assertEqual(self.calc.subtract(4, 1),3)
+         self.assertEqual(self.calc.subtract(-3, -3), 0)
+         self.assertEqual(self.calc.subtract(5, -5), 10)
+         self.assertEqual(self.calc.subtract(10, 0), 10)
+         self.assertEqual(self.calc.subtract(4, "b"), 1)
+         self.assertEqual(self.calc.subtract(None))
        except TypeError as tt:
           print(tt)
 
     def test_multiply(self):
        try:
-          self.assertEqual(SimpleCalculator(0, 5), 0)
-          self.assertEqual(SimpleCalculator(5, -2), -10)
-          self.assertEqual(SimpleCalculator(3, 2), 6)
-          self.assertEqual(SimpleCalculator(-4, -4), 16)
-          self.assertEqual(SimpleCalculator("c", 4))
-          self.assertEqual(SimpleCalculator(None))
+          self.assertEqual(self.calc.multiply(0, 5), 0)
+          self.assertEqual(self.calc.multiply(5, -2), -10)
+          self.assertEqual(self.calc.multiply(3, 2), 6)
+          self.assertEqual(self.calc.multiply(-4, -4), 16)
+          self.assertEqual(self.calc.multiply("c", 4), 8)
+          self.assertEqual(self.calc.multiply(None))
        except TypeError as err:
           print(err)
 
     def test_divide(self):
        try:
-          self.assertEqual(SimpleCalculator(10, 2), 5)
-          self.assertEqual(SimpleCalculator(0, 3), 0)
-          self.assertEqual(SimpleCalculator(4, 0))
-          self.assertEqual(SimpleCalculator(10, -5), -2)
-          self.assertEqual(SimpleCalculator("d", 10))
-          self.assertEqual(SimpleCalculator(None))
+          self.assertEqual(self.calc.divide(10, 2), 5)
+          self.assertEqual(self.calc.divide(0, 3), 0)
+          self.assertEqual(self.calc.divide(4, 0))
+          self.assertEqual(self.calc.divide(10, -5), -2)
+          self.assertEqual(self.calc.divide("d", 10), 2)
+          self.assertEqual(self.calc.divide(None))
        except ZeroDivisionError as zer:
           print(zer)
        except TypeError as te:
