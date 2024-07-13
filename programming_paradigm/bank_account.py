202f90e657cb50b361c6056e3452bea590ee73a9
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-13 12:59:41 +0000

    added new files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+# Programming Paradigms & Exception handling
+# Task 0 - Create a Simple Bank Account Class
+
+# Class Definition
+class BankAccount():
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+
+# Encapsulation and Behaviors
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount < self.account_balance:
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+            
