e3c9cacede5af197d8f3c1bc3e486281f95c23ad
Ernest Mimshach <mimshach.e@gmail.com>
2024-07-21 21:12:56 +0000

    added new files

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+# Task 2 - Exploring Polymorphism and Method Overriding
+import math
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * pow(self.radius, 2) # the pow() in-built function is for squaring the base number
